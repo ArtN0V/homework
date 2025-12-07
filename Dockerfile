b5acacaac92a40a057f501d7c51a3e7a16df86ec
ArtN0V <artemnovikov02@yandex.ru>
2025-12-08 01:04:17 +0300

    little fixes

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17-jdk-slim-buster
-WORKDIR /app
-COPY target/MyHomeWork-1.0-SNAPSHOT.jar /app/app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- discovery-server/src/main/java/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/discoveryserver/DiscoveryServerApplication.java
@@ -1,8 +1,13 @@
+package discoveryserver;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration.class
+})
 @EnableEurekaServer
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
